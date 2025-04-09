ec32e7408da5cbbe1a9ec6addda84f2892874fee
Tirth-2504 <tirthpatel2130@gmail.com>
2025-04-08 18:38:04 -0600

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,3 @@
-const fs  = require('fs');
-const filePath = require('../database.json'); 
-const { readData, writeData } = require('../utils/file');  
-
-async function createUser(req, res){
-    try{
-        const data = await readData();
 const fs = require('fs');
 const filePath = require('../database.json');
 const { readData, writeData } = require('../utils/file.js');
@@ -34,37 +27,11 @@ async function createUser(req, res){
         //commits data by writing it to file.
         await writeData(data);
 
+        readData.redirect('/home');
+
     } catch (error) {
         res.status(500).json(`Internal Server Error: ${error}`);
     }
 }
 
 module.exports = {createUser};
-        //determine the last user ID
-        const lastUser = data.users[data.users.length - 1];
-        
-        //what happens if there are no users?
-        //ternary operator
-        const nextId = lastUser ? lastUser.id + 1 : 1;
-
-        //create the new user object 
-        const newUser = {
-            id: nextId,
-            username: req.body.username,
-            firstname: req.body.first_name,
-            email: req.body.email,
-        }
-
-        //push the new data to memory object
-        data.users.push(newUser);
-
-        //commit data by writing it to file.
-        await writeData(data);
-
-    }catch(error){
-        res.status(500).json(`Internal Server Error: ${error}`);
-    }
-
-}
-
-module.exports = { createUser };
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController.js');
+const { readData } = require('../utils/file.js');
 
+router.use(express.json());
+//Middleware to serve static files
+router.use(express.urlencoded({extended: true}));
 
 //Routes handlers for our home page
 router.get('/home', (req, res) => {
     res.render('home');
 });
 
-router.use(express.urlencoded({extended: true}));
+//api endpointexposing the users resource
+router.get('/api/v1/users', async (req, res) => {
+    try{
+        const data = await readData();
+
+        res.json(data.users);
+
+    }catch(error){
+        res.status(500).json('Internal Server Error');
+    }
+});
 //Users post route to add a new user
 router.post('/users',userController.createUser);
 
